19b423297bbb6b70d504ab55d7a121bd11eef1c4
EduardoMnds <eduardo2002brasil@gmail.com>
2025-05-23 20:49:43 -0300

    GET lists

---
+++ src/main/java/com/eduardoit/dslist/repository/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.eduardoit.dslist.repository;
+
+import com.eduardoit.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+
+
+}
